b11aac235a902de6b42a62aee2a7e812db2f1e64
JKerving <zjw18603166118@sina.cn>
2017-06-19 23:50:31 +0800

    新增chooseBestFeatureToSplit函数，用于选取划分数据集的最佳特征值

--- Ch3/trees.py
+++ Ch3/trees.py
@@ -14,8 +14,6 @@ def createDataSet():
     return dataSet, labels
 
 
-
-
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
@@ -35,11 +33,32 @@ myDat, labels = createDataSet()
 shannonEnt = calcShannonEnt(myDat)
 print(shannonEnt)
 
+
 def splitDataSet(dataSet, axis, value):
     retDataSet = []
     for featVec in dataSet:
         if featVec[axis] == value:
             reducedFeatVec = featVec[:axis]
             reducedFeatVec.extend(featVec[axis + 1:])
-            append = retDataSet.append(reducedFeatVec)
+            retDataSet.append(reducedFeatVec)
     return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
