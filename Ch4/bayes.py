e9b1f857d56bd5cdb4025a1ed26a280c1af7d065
JKerving <zjw18603166118@sina.cn>
2017-06-26 00:37:36 +0800

    Begin Bayes

---
+++ Ch4/bayes.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def loadDataSet():
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postingList, classVec
+
+
+def createVocabList(dataSet):
+    vocaSet = set([])
+    for document in dataSet:
+        vocaSet = vocaSet | set(document)
+    return list(vocaSet)
+
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print('the word:%s is not in my Vocabulary!' % word)
+    return returnVec
+
+
+listOPosts, listClasses = loadDataSet()
+myVocabList = createVocabList(listOPosts)
+print(myVocabList)
+returnVec = setOfWords2Vec(myVocabList, listOPosts[0])
+print(returnVec)
