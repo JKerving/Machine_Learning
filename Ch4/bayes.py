221a9661aea3b55e0cb424983fdaca1c2a51978e
JKerving <zjw18603166118@sina.cn>
2017-06-27 00:08:33 +0800

    添加朴素贝叶斯分类函数

--- Ch4/bayes.py
+++ Ch4/bayes.py
@@ -37,10 +37,10 @@ def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory) / float(numTrainDocs)
-    p0Num = zeros(numWords)
-    p1Num = zeros(numWords)
-    p0Denom = 0.0
-    p1Denom = 0.0
+    p0Num = ones(numWords)
+    p1Num = ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -48,8 +48,8 @@ def trainNB0(trainMatrix, trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = p1Num / p1Denom
-    p0Vect = p0Num / p0Denom
+    p1Vect = log(p1Num / p1Denom)
+    p0Vect = log(p0Num / p0Denom)
     return p0Vect, p1Vect, pAbusive
 
 
@@ -57,6 +57,14 @@ trainMat = []
 for postinDoc in listOPosts:
     trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
 
-p0V,p1V,pAb = trainNB0(trainMat,listClasses)
-print(pAb)
+p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+print(p0V)
 
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + log(1 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
